6b003fb179f5cdff2d098761760c30dda2b21e2a
Ashura <Ashurajohnson3417@gmail.com>
2016-06-20 15:35:01 -0400

    Library class and status enum

---
+++ src/LibraryItem.java
@@ -0,0 +1,34 @@
+
+public abstract class LibraryItem {
+	private String title;
+	private String author;
+	private status status;
+	private String dueDate;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public status getStatus() {
+		return status;
+	}
+	public void setStatus(status status) {
+		this.status = status;
+	}
+	public String getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+	
+}
